e3fc11aa4d8db815925233f1d390f445ce1adca5
kei <kei.adc11.29@gmail.com>
2020-03-17 03:04:52 +0900

    get title スクレイピングの練習 サイトのタイトル取得

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+       try {
+           String url = "https://google.com";
+//           Document doc = Jsoup.connect("https://www.yahoo.co.jp/").get();
+
+           Document doc = Jsoup.connect(url).get();
+           Elements elm = doc.select("title");
+
+           for(Element elms: elm){
+               String title = elms.text();
+               System.out.println(title);
+           }
+
+       } catch(IOException e){
+           e.printStackTrace();
+       }
     }
 }
